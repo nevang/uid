9f1fb0686a7a9d07c6bc4f9e611812eb9fa63355
Nikolas Evangelopoulos <nikolas@jkl.gr>
2013-01-07 19:02:17 +0200

    Finalize crossbuilds

--- project/Build.scala
+++ project/Build.scala
@@ -10,31 +10,39 @@ object Settings {
     organization := buildOrganization,
     version      := buildVersion,
     scalaVersion := buildScalaVersion,
-    crossScalaVersions := Seq("2.9.2", "2.9.1", "2.9.0-1", "2.8.2", "2.8.1"))
+    crossVersion := CrossVersion.binary,
+    crossScalaVersions := Seq("2.10.0", "2.9.3-RC1", "2.9.2", "2.9.1-1", "2.9.1", "2.9.0-1", "2.9.0", "2.8.2", "2.8.1"))
 
   val defaultSettings = buildSettings ++ Seq(
     resolvers ++= DefaultOptions.resolvers(true),
-    scalacOptions in Compile ++= Seq("-encoding", "UTF-8", "-deprecation", "-unchecked"),
+    scalacOptions in (Compile, compile) ++= Seq("-encoding", "UTF-8", "-deprecation", "-unchecked"),
     shellPrompt := DefaultOptions.shellPrompt(buildVersion))
 }
 
 object Version {
-  val scala     = "2.9.2"
-  val scalaTest = "1.8"
-  val caliper   = "0.5-rc1"
-  val eaio      = "3.2"
+  def scalaTest(scalaVersion: String) = scalaVersion match {
+    case "2.10.0" => "1.9.1"
+    case _        => "1.8"
+  } 
+
+  val scala   = "2.9.2"
+  val caliper = "0.5-rc1"
+  val eaio    = "3.2"
 }
 
 object Dependency {
-  val scalaTest = "org.scalatest"      %% "scalatest" % Version.scalaTest % "test"
-  val caliper   = "com.google.caliper" %  "caliper"   % Version.caliper
-  val eaio      = "com.eaio.uuid"      %  "uuid"      % Version.eaio
+  def scalaTest(scalaVersion: String) = 
+    "org.scalatest" %% "scalatest" % Version.scalaTest(scalaVersion) % "test"
+
+  val caliper = "com.google.caliper" %  "caliper" % Version.caliper
+  val eaio    = "com.eaio.uuid"      %  "uuid"    % Version.eaio
+
 }
 
 object Dependencies {
   import Dependency._
 
-  val core = Seq(scalaTest)
+  def core(scalaVersion: String) = Seq(scalaTest(scalaVersion))
 
   val benchmark = Seq(caliper, eaio)
 }
@@ -92,7 +100,7 @@ object UIDBuild extends Build {
     base = file("."),
     settings = defaultSettings ++ publishSettings ++ Seq(
       description := "64-bit Ids for Scala",
-      libraryDependencies ++= Dependencies.core,
+      libraryDependencies <++= scalaVersion(Dependencies.core(_)),
       parallelExecution in Test := false,
       testOptions in Test += Tests.Argument("-oDF"),
       scalacOptions in (Compile, doc) ++= DefaultOptions.scaladoc("UID", buildVersion)))
