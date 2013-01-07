395c9a34d50712156e7ea356f404a19766711c44
Nikolas Evangelopoulos <nikolas@jkl.gr>
2013-01-07 02:51:32 +0200

    Polish codec

--- src/main/scala/gr/jkl/uid/Codec.scala
+++ src/main/scala/gr/jkl/uid/Codec.scala
@@ -1,73 +1,96 @@
 package gr.jkl.uid
 
 import java.lang.{ StringBuilder => JStringBuilder }
-import scala.collection.immutable.{TreeMap, BitSet}
+import scala.collection.immutable.{ TreeMap, BitSet }
 
 /** Base64 like encoding algorithm. */
 private[uid] object Codec {
   
-  /** Sorted set of the url safe base 64 chars. */
-  private[this] val lexicon = 
+  /** Sorted url-safe Chars. 
+    *
+    * Encoding Chars, safe for URIs. Characters are sorted. Consequently 
+    * unsigned Longs will be sorted on the same way.
+    *
+    * == RFC 2396 ==
+    * unreserved = alphanum | mark
+    * mark       = "-" | "_" | "." | "!" | "~" | "*" | "'" | "(" | ")"
+    *
+    * == RFC 1808 & RFC 1738 ==
+    * unreserved  = alpha | digit | safe | extra
+    * safe        = "$" | "-" | "_" | "." | "+"
+    * extra       = "!" | "*" | "'" | "(" | ")" | ","
+    *
+    * @see [[ftp://ftp.funet.fi/pub/doc/rfc/rfc1738.txt RFC 1738]]
+    * @see [[ftp://ftp.funet.fi/pub/doc/rfc/rfc1808.txt RFC 1808]]
+    * @see [[ftp://ftp.funet.fi/pub/doc/rfc/rfc2396.txt RFC 2396]]
+    */
+  private[this] val chars = 
     "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz"
 
-  /** Lexicon as StringBuilder. */
-  private[this] val lexiconSB = new JStringBuilder(lexicon)
+  /** Index as StringBuilder. */
+  private[this] val index = new JStringBuilder(chars)
 
   /** Map of chars and their corresponding values. */
-  private[this] val lexiconMap = 
-    TreeMap(lexicon.map(c => (c, lexicon.indexOf(c).toLong)) :_*) 
+  private[this] val reverseIndex = 
+    TreeMap(chars.map(c => (c, chars.indexOf(c).toLong)) :_*) 
 
-  /** Set of the int representations of the chars. */
-  private[this] val lexiconCharSet = BitSet(lexicon.map(_.toInt) :_*)
+  /** Set of the Chars encoded bytes as Ints. */
+  private[this] val byteSet = BitSet(chars.map(_.toInt) :_*)
 
-  /** Returns a ZeroBuilder representing the 0. */
-  private[this] def zeroSB = new JStringBuilder("-----------")
+  /** 6-bit zero as Char. */
+  private[this] val zeroChar = chars.head
+
+  /** 64-bit unsigned zero as String. */
+  private[this] val zeroString = zeroChar * 11
+
+  /** Returns a StringBuilder representing 0. */
+  private[this] def zeroSB = new JStringBuilder(zeroString)
 
   /** Encodes the given Long. */
-  private[uid] def encode(long: Long): String = {
+  private[uid] def encode(value: Long): String = {
     val sb = new JStringBuilder(11)
     sb.setLength(11)
-    sb.setCharAt(10, lexiconSB.charAt((long        & 63L).toInt))
-    sb.setCharAt( 9, lexiconSB.charAt((long >>>  6 & 63L).toInt))
-    sb.setCharAt( 8, lexiconSB.charAt((long >>> 12 & 63L).toInt))
-    sb.setCharAt( 7, lexiconSB.charAt((long >>> 18 & 63L).toInt))
-    sb.setCharAt( 6, lexiconSB.charAt((long >>> 24 & 63L).toInt))
-    sb.setCharAt( 5, lexiconSB.charAt((long >>> 30 & 63L).toInt))
-    sb.setCharAt( 4, lexiconSB.charAt((long >>> 36 & 63L).toInt))
-    sb.setCharAt( 3, lexiconSB.charAt((long >>> 42 & 63L).toInt))
-    sb.setCharAt( 2, lexiconSB.charAt((long >>> 48 & 63L).toInt))
-    sb.setCharAt( 1, lexiconSB.charAt((long >>> 54 & 63L).toInt))
-    sb.setCharAt( 0, lexiconSB.charAt((long >>> 60      ).toInt))
+    sb.setCharAt(10, index.charAt((value        & 63L).toInt))
+    sb.setCharAt( 9, index.charAt((value >>>  6 & 63L).toInt))
+    sb.setCharAt( 8, index.charAt((value >>> 12 & 63L).toInt))
+    sb.setCharAt( 7, index.charAt((value >>> 18 & 63L).toInt))
+    sb.setCharAt( 6, index.charAt((value >>> 24 & 63L).toInt))
+    sb.setCharAt( 5, index.charAt((value >>> 30 & 63L).toInt))
+    sb.setCharAt( 4, index.charAt((value >>> 36 & 63L).toInt))
+    sb.setCharAt( 3, index.charAt((value >>> 42 & 63L).toInt))
+    sb.setCharAt( 2, index.charAt((value >>> 48 & 63L).toInt))
+    sb.setCharAt( 1, index.charAt((value >>> 54 & 63L).toInt))
+    sb.setCharAt( 0, index.charAt((value >>> 60      ).toInt))
     sb.toString
   }
 
   /** Encodes the given Long omitting 0s from the beginning. */
-  private[uid] def shortEncode(long: Long): String = 
-    if(long == 0L) "-"
-    else encode(long).dropWhile(_ == '-')
+  private[uid] def shortEncode(value: Long): String = 
+    if(value == 0L) zeroChar.toString
+    else encode(value).dropWhile(_ == zeroChar)
 
   /** Checks if a String can be decoded to Long. */
-  private[uid] def isValid(str: String): Boolean = 
-    str.length < 12 && str.forall(c => lexiconCharSet.contains(c.toInt))
+  private[uid] def isValid(value: String): Boolean = 
+    value.length < 12 && value.forall(c => byteSet.contains(c.toInt))
 
   /** Decodes a String, shortened or not, to Long. */
-  private[uid] def decode(str: String): Option[Long] = 
-    if (isValid(str)) Some(decodeFull(
-      if (str.length == 11) str
-      else zeroSB.replace(11 - str.length, 11, str).toString))
+  private[uid] def decode(value: String): Option[Long] = 
+    if (isValid(value)) Some(decodeFull(
+      if (value.length == 11) value
+      else zeroSB.replace(11 - value.length, 11, value).toString))
     else None
 
   /** Decodes a String with 11 characters to Long. */
-  private[this] def decodeFull(str: String): Long =
-    (lexiconMap(str( 0)) << 60) |
-    (lexiconMap(str( 1)) << 54) |
-    (lexiconMap(str( 2)) << 48) |
-    (lexiconMap(str( 3)) << 42) |
-    (lexiconMap(str( 4)) << 36) |
-    (lexiconMap(str( 5)) << 30) |
-    (lexiconMap(str( 6)) << 24) |
-    (lexiconMap(str( 7)) << 18) |
-    (lexiconMap(str( 8)) << 12) |
-    (lexiconMap(str( 9)) <<  6) |
-     lexiconMap(str(10)) 
+  private[this] def decodeFull(value: String): Long =
+    (reverseIndex(value( 0)) << 60) |
+    (reverseIndex(value( 1)) << 54) |
+    (reverseIndex(value( 2)) << 48) |
+    (reverseIndex(value( 3)) << 42) |
+    (reverseIndex(value( 4)) << 36) |
+    (reverseIndex(value( 5)) << 30) |
+    (reverseIndex(value( 6)) << 24) |
+    (reverseIndex(value( 7)) << 18) |
+    (reverseIndex(value( 8)) << 12) |
+    (reverseIndex(value( 9)) <<  6) |
+     reverseIndex(value(10)) 
 }
