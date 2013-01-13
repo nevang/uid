21d451023964ae80da22d5509762af3f1b48f333
Nikolas Evangelopoulos <nikolas@jkl.gr>
2013-01-13 11:23:00 -0800

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  var isStable, apiLink
+
+  isStable = function(v) {
+    return /^[\d\.\-]+$/i.test(v)
+  }
+
+  apiLink = function(v) {
+    return 'api/' + (isStable(v) ? v : latest) + '/gr/jkl/uid/package.html'
+  }
+
+  $.get('releases.xml', function(data) {
+    $(data).find('release').each(function(){
+      var v, h, a
+      v = $(this).find('version').text()
+      h = $('<li />')
+      h.append('<a href="' + apiLink(v) + '">ScalaDoc for uid ' + v + '</a>')
+      a = isStable(v) ? '#stables' : '#snapshots'
+      h.appendTo($(a))
+    })
+  }, 'xml')
+})
